f2deb27846c9e5173b6988c7daa7d08385279623
Kwynn <buessresume@gmail.com>
2020-10-19 03:24:04 -0400

    and more whittling

--- base.php
+++ base.php
@@ -6,14 +6,18 @@ require_once('parse.php');
 class wsal_load_and_parse {
 
     const alpath  = '/tmp/access.log';
-    const linesAfter = '2020-09-15 19:30:00';
+    const linesAfter = '2020-10-15 19:30:00';
     const maxLines2PowOf = 45;
 
-    public function __construct() { $this->getLineOfDate(strtotime(self::linesAfter)); }
+    public function __construct() { 
+	$this->setTotLines();
+	$this->getLineOfDate(strtotime(self::linesAfter)); 
+    }
+    
+    private function setTotLines() { $this->grtotl = intval(trim(shell_exec('wc -l < ' . self::alpath))); }
     
     private function getLineOfDate($tsa) {
-	$totn = intval(trim(shell_exec('wc -l < ' . self::alpath)));
-	$this->grtotl = $nxt = $imaxp = $totn; unset($totn);
+	$nxt = $imaxp = $this->grtotl;
 	$iminp = 0; self::avg($nxt, $iminp, $ignore); unset($ignore);
 	for ($i=0; $i < self::maxLines2PowOf && $nxt < $imaxp; $i++) 
 	    if (wsalParseOneLine($this->getLine($nxt)) >= $tsa) self::avg($nxt, $iminp, $imaxp);
@@ -21,7 +25,7 @@ class wsal_load_and_parse {
 	return $nxt;
     }
     
-    private static function avg   (&$a, $b, &$eref) { $eref = $a; $a = intval(ceil(($a + $b) / 2)); } 
+    private function avg    (&$a, $b, &$eref) { $eref = $a; $a = intval(ceil(($a + $b) / 2)); } 
     private function getLine($i) { return trim(shell_exec('tail -n ' . ($this->grtotl - $i + 1) . ' 2> /dev/null ' . self::alpath . ' | head -n 1 ')); }
 }
 
