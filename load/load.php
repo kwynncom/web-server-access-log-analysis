47998d752034f2c9bba9c5ad1c4b3d5ab7154619
Kwynn <buessresume@gmail.com>
2020-11-14 00:56:15 -0500

    de-duplication working or at least close

--- load/load.php
+++ load/load.php
@@ -16,28 +16,43 @@ class wsal_load {
 	$this->doit();
     }
     
-    private function doit() {
+    private function getPaths() {
 	$inpath = '/home/' . get_current_user() . self::inpathsfx;
 	$c = 'find ' . $inpath . " -type f -name 'acc*' " . ' -printf  "%T+\t%p\n" ' . ' | ' . ' sort ';
 	$list = trim(shell_exec($c));
 	$als  = explode("\n", $list);
+	foreach($als as $timeAndPath) {
+	    preg_match('/^\S+\s+(.*(\.[^\.]+))$/', $timeAndPath, $m10);	    
+	    kwas(isset($m10[2]), 'match failed m10 cpFiles10 load wsal');
+	    $path = $m10[1];
+	    $key = '.bz2';
+	    if ($m10[2] === $key) {
+		shell_exec("bzip2 -d $path");
+		$path = preg_replace('/\.bz2$/', '', $path);
+	    }
+	    
+	    $paths[] = $path;
+	}
 	
-	foreach($als as $i => $timeAndPath) {
-	    
-	    preg_match('/^\S+\s+(.*(\.[^\.]+))$/', $timeAndPath, $m10);
-	    kwas(isset($m10[1]), 'match failed m10 cpFiles10 load wsal');
-	    
-	    $p = $m10[1];
-	    $fia = $this->meta->rdAndCkFile($p);
+	return $paths;
+    }
+    
+    private function doit() {
+	foreach(self::getPaths() as $path) {
+	    $fia = $this->meta->rdAndCkFile($path);
 	    if ($fia === true) continue; 
 	    
-	    $this->forkDoPath = $p;
+	    $this->forkDoPath = $path;
 	    $this->totLines = $fia['lines'];
+	    
 	    if (1) {
-		fork::dofork([$this, 'childrenRun'], 1, $this->totLines);
-	    	$fia = $this->meta->rdAndCkFile($p);
+		
+		if ($fia['status'] === 'partial') $startAt = $fia['startAt'];
+		else $startAt = 1;
+		fork::dofork([$this, 'childrenRun'], $startAt, $this->totLines);
+	    	$fia = $this->meta->rdAndCkFile($path);
 		kwas($fia === true, 'not loaded status wsal');
-	        echo($this->totLines . ' lines loaded from ' . $p . "\n");
+	        echo($this->totLines . ' lines loaded from ' . $path . "\n");
 	    }
 	    
 	    continue;
--- load/meta.php
+++ load/meta.php
@@ -1,38 +1,60 @@
 <?php
 
 class wsal_meta extends dao_wsal {
+    
+    const pends = 'pending';
+    
     public function __construct() {
 	parent::__construct(self::db);	
 	$this->fcoll    = $this->client->selectCollection(self::db, 'files');
+	$this->lcoll->createIndex(['n' => 1]);
     }
     
-    private function ck($hl, $tl, $hds, $tds, $nin) {
+    private function ck($hl, $tl, $hds, $tds, $nin, $path) {
 	
 	$hr = $this->lcoll->count(['n' => 1   , 'line' => $hl]);
 	$tr = $this->lcoll->count(['n' => $nin, 'line' => $tl]);
+	
+	if ($hr === 1 && $tr === 0) {
+	    $fr = $this->fcoll->findOne(['headds' => $hds]);
+	    if (!$fr) return self::pends;
+	    $prevLines = $fr['lines'];
+	    $ckTail = $nin - $prevLines + 1;
+	    $cmd = "tail -n $ckTail $path  | head -n 1";
+	    $prl = trim(shell_exec($cmd));
+	    $pra = wsalParseOneLine($prl);
+	    if ($pra['dates'] === $fr['tailds']) return ['status' => 'partial', 'startAt' => $prevLines + 1];
+	}
+	
 	$q = ['n'  => ['$gte' => 1, '$lte' => $nin],
 	      'ts' => ['$gte' => strtotime($hds), '$lte' => strtotime($tds)]
 	    ];
 	$lr = $this->lcoll->count($q);
 	if ($hr + $tr === 2 && $lr === $nin) return 'loaded';
-	return 'pending';
+	return self::pends;
     }
     
     public function rdAndCkFile($path) {
 	$head  = self::ht('head', $path);
 	$tail  = self::ht('tail', $path);
-	$lines = self::lines($path); unset($path);
+	$lines = self::lines($path);
 	$headpa = wsalParseOneLine($head);
 	$tailpa = wsalParseOneLine($tail);
 	$headds = $headpa['dates']; unset($headpa);
 	$tailds = $tailpa['dates']; unset($tailpa);
 	$hts = strtotime($headds);
 	$_id = date('m-d', $hts); unset($hts);
-	$status = $this->ck($head, $tail, $headds, $tailds, $lines); unset($head, $tail);
-	$dat = get_defined_vars();	
-	$this->fcoll->upsert(['_id' => $_id], $dat);
+	$status = $this->ck($head, $tail, $headds, $tailds, $lines, $path); unset($head, $tail, $path);
+	$qkey = ['_id' => $_id];
+	if (is_array($status)) {
+	    extract($status);
+	    $dbdat = ['status' => $status];
+	    $vars = get_defined_vars();
+	} else $vars = $dbdat = get_defined_vars();
+	$this->fcoll->upsert($qkey, $dbdat);
+	
 	if ($status === 'loaded') return true;
-	return $dat;
+	return $vars;
     }
     
     public static function lines($path) { 
