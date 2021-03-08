0e6f49e519e25b6f595914358520a8f5e2f57396
Kwynn <buessresume@gmail.com>
2021-03-07 22:03:08 -0500

    test / temp file starting to work

--- files/loadlive.php
+++ files/loadlive.php
@@ -7,12 +7,13 @@ class load_wsal_live extends dao_generic_2 {
 
     const dbName = 'wsalogs';
     const basecmd = '/var/kwynn/goa ';
+    const ttpf = '/tmp/tl.log';
     
     public function __construct() {
 	parent::__construct(self::dbName, __FILE__);
 	$this->creTabs(['l' => 'lines']);
 	$thei = $this->lcoll->createIndex(['md5' => 1, 'i' => 1], ['unique' => true]);
-	$this->l10();
+	$this->l05();
 	return;
     }
     
@@ -24,7 +25,33 @@ class load_wsal_live extends dao_generic_2 {
 	$n10 = $maxlv - $maxdb;
 	$n = $n10 + 20;
 	$l = shell_exec(self::basecmd . "'" . "tail -n $n /var/log/apache2/access.log" . "'");
+	
+	$fp = self::ttfp;
+	$fp1r = file_put_contents($fp, '');
+	kwas(chmod($fp, 0600), 'chmod fail');
+	file_put_contents($fp, $l);
 	echo($l);
+	return $l;
+	
+    }
+    
+    private function l05() {
+	if (file_exists(self::ttpf)) $lss = file_get_contents(self::ttpf);
+	else $lss = $this->l10();
+	$this->match($lss);
+    }
+    
+    
+    private function match($lss) { 
+	$dat = [];
+	$lsa = explode("\n", $lss);
+	$len = count($lsa);
+	
+	for ($i=0; $i + 1 < $len; $i++) {
+	    $md50 = md5($lsa[$i]);
+	    // $this->
+	    
+	}
 	
     }
     
