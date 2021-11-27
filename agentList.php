3e388cff076a8d511e905a3b84ac3fc884b4fa6a
kwynncom <buessresume@gmail.com>
2021-11-26 19:56:13 -0500

    sorted counted agents in DB

--- agentList.php
+++ agentList.php
@@ -9,26 +9,52 @@ class agents extends dao_generic_3 {
 	
 	public function __construct() {
 		parent::__construct(self::dbname);
-		$this->creTabs(['l' => 'lines', 'm' => 'meta']);
+		$this->creTabs(['l' => 'lines', 'm' => 'meta', 'a' => 'agents']);
+		if (!isAWS() && 1) $this->acoll->drop();
+		$this->acoll->createIndex(['agent' => 1], ['unique' => true]);
 		$this->ag10();
+		$this->ag20();
+	}
+	
+	private function ag20() {
+		$a = $this->aga;
+		
+		$now = time();
+		$dHu = date('r', $now);
+		$did = date('Y-m-d-Hi-s');
+		
+		$oi = 0;
+		
+		foreach($a as $ag => $n) {
+			$d['agent'] = $ag;
+			$d['n']     = $n;
+			$d['ts_calced'] = $now;
+			$d['r_calced' ] = $dHu;
+			
+			$d['_id'] = 'o-' . ++$oi . '-n-' . $n . '-' . $did;
+			
+			$this->acoll->insertOne($d);
+			
+		}
+		
 	}
 	
 	private function ag10() {
 		
-		$totn = $this->lcoll->count();
-		$noen = $this->lcoll->find(['iserr' => false]);
+		$noea = $this->lcoll->find(['iserr' => false]);
 		
-		foreach($noen as $r) {
-			if ($r['iserr']) continue;
+		$this->lnpn = count($noea);
+		
+		foreach($noea as $r) {
 			$agr = $r['agent'];
 			$ag = $agr;
 			if (!isset($a[$ag])) $a[$ag] = 0;
 			$a[$ag]++;
-		}
+		} unset($r, $ag, $agr);
 		
-		asort($a);
-		
-		var_dump($a);
+		arsort($a);
+
+		$this->aga = $a;
 	}
 	
 	
