2c5d4fec637df6212fe600d33d575fa91d51ac48
kwynncom <buessresume@gmail.com>
2021-12-03 00:55:32 -0500

    bot definitions

--- bots/bots.php
+++ bots/bots.php
@@ -6,12 +6,39 @@ class wsal_bots {
 	public static function isBot($ain) {
 		if (stm($ain, 'external')) return FALSE;
 		if (self::preciseList($ain)) return TRUE;
+		if (self::pandre($ain)) return TRUE;
+		return FALSE;
 	}
 	
 	private static function preciseList($ain) {
 		$ss = ['(compatible; PetalBot;+https://webmaster.petalsearch.com/site/petalbot)'];
 		foreach($ss as $s) if (stm($ain, $s)) return TRUE;
 		return FALSE;
+	}
+	
+	private static function pandre($ain) {
+		$a = [
+			['(compatible; AhrefsBot/','v','; +http://ahrefs.com/robot/)']
+		];
+		
+		$h = $ain;
+		foreach($a as $r) {
+			foreach($r as $part) {
+				if ($part !== 'v') {
+					$sp = strpos($h, $part);
+					if ($sp === false) continue 2;
+					$h = substr($h, $sp + strlen($part));
+				} else {
+					if (!preg_match('/^\d[^;]*/', $h, $ms)) continue 2;
+					$h = substr($h, strlen($ms[0]));
+				}
+			}
+			
+			return TRUE;
+		}
+		
+		return FALSE;
+				
 	}
 }
 
