2567f8763bd38c769593e2a3e621c70e1f95694b
kwynncom <buessresume@gmail.com>
2021-12-22 21:01:45 -0500

    my own usage filter seems to be working; starting to filter more

--- views/view.php
+++ views/view.php
@@ -2,6 +2,7 @@
 
 require_once(__DIR__ . '/../load/dao_wsal.php');
 require_once(__DIR__ . '/../bots/bots.php');
+require_once(__DIR__ . '/myips.php');
 
 class wsal_view extends dao_wsal {
 	
@@ -10,7 +11,8 @@ class wsal_view extends dao_wsal {
 	public function __construct() {
 		parent::__construct();
 		$this->db10();
-		$this->do10();
+		$a20 = $this->do10();
+		$this->do20($a20);
 	}
 	
 	private function db10() {
@@ -19,12 +21,27 @@ class wsal_view extends dao_wsal {
 	}
 	
 	private function do10() {
+		
+		$ia = myips::get();
+		
 		$res = $this->lcoll->find(['ts' => ['$gt' => time() - self::defaultSBack]], ['sort' => ['tsus' => 1, 'n' => 1]]);
 		foreach($res as $a) 
-			if (!wsal_bots::isBot($a['agent'])) 
-				if ($a['httpCode'] < 400) 
-					echo($a['wholeLine'] . "\n");
-		return;
+			if (!wsal_bots::isBot($a['agent']) && $a['httpCode'] <= 399) {
+				$ism = isset($ia[$a['ip']][$a['agent']]);
+				if ($ism) continue;// echo('KWU ');
+					
+				// echo($a['wholeLine'] . "\n");
+				$a20[$a['ip']][] = $a;
+			}
+		return $a20;
+	}
+	
+	private function do20($a) {
+		foreach($a as $ip => $ipa) 
+			foreach($ipa as $r)
+				echo($r['wholeLine'] . "\n");
+		
+		
 	}
 	
 }
