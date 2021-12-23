799101c05ae60441328d317dd2b58e3870a81599
kwynncom <buessresume@gmail.com>
2021-12-22 20:25:50 -0500

    starting to ID my own rows

--- views/view.php
+++ views/view.php
@@ -5,13 +5,21 @@ require_once(__DIR__ . '/../bots/bots.php');
 
 class wsal_view extends dao_wsal {
 	
+	const defaultSBack = 110000;
+	
 	public function __construct() {
 		parent::__construct();
+		$this->db10();
 		$this->do10();
 	}
 	
+	private function db10() {
+		$res = $this->lcoll->createIndex(['ts' => 1]);
+		return;
+	}
+	
 	private function do10() {
-		$res = $this->lcoll->find([], ['sort' => ['tsus' => 1, 'n' => 1]]);
+		$res = $this->lcoll->find(['ts' => ['$gt' => time() - self::defaultSBack]], ['sort' => ['tsus' => 1, 'n' => 1]]);
 		foreach($res as $a) 
 			if (!wsal_bots::isBot($a['agent'])) 
 				if ($a['httpCode'] < 400) 
