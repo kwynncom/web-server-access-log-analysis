37f000f43b562d3737f12d2d58194bc7555145fb
kwynncom <buessresume@gmail.com>
2022-02-05 01:10:32 -0500

    strtok version

--- forks/load20Test.php
+++ forks/load20Test.php
@@ -5,7 +5,7 @@ require_once('ranges.php');
 
 class load20_divide {
 	
-	const lfin = '/tmp/a26.log';
+	const lfin = '/tmp/access.log';
 	const chunksM = 10;
 	const chunksb = self::chunksM * M_MILLION;
 	const maxCh   = 500;
@@ -16,35 +16,33 @@ class load20_divide {
 		$rs = multi_core_ranges::get(1, $sz);
 		$r = fopen(self::lfin, 'r');
 		$rn = 0;
-		$si = 0;
-		$ii = 0;
+
 		$remn = $sz;
-		$remsa = [];
 		
-		while($si++ < self::maxCh && $rn < $sz) {
+		for ($ri=0; $ri++ < self::maxCh && $rn < $sz; $ri++) {
 			if ($remn > self::chunksb) $tor = self::chunksb;
 			else					   $tor = $remn;
 			
+			fseek(     $r, $rn);
 			$t = fread($r, $tor); kwas(kwifs($t, $tor - 1) !== false && kwifs($t, $tor) === false, 'bad read n 2317');
 			$rn   += $tor;
 			$remn -= $tor; unset($tor);
 			
+			$line = strtok($t, "\n");
+
+			while ($line !== false) {
+				# do something with $line
+				$line = strtok("\n");
+			} 
 			
-			$a = explode("\n", $t); unset($t);
-			if ($ii > 0) {
-				$remsa[$rn]['f'] = $a[0]; 
-				unset(			   $a[0]);
-			}
-			$lan = count($a) - 1;
-			$remsa[$rn]['l'] = $a[$lan];
-			unset(			   $a[$lan]); unset($lan);
-	
-			
-			$ii++;
 			continue;
 		} 
 		return; 
 		
+		
+	}
+	
+	function read() {
 		
 	}
 
