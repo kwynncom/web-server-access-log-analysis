41a027e7fc312c6178c1929933f71f6d2ee4737f
kwynncom <buessresume@gmail.com>
2022-02-04 23:26:09 -0500

    divide loop starting to work

--- forks/load20Test.php
+++ forks/load20Test.php
@@ -7,20 +7,36 @@ class load20_divide {
 	
 	const lfin = '/tmp/access.log';
 	const chunksM = 10;
+	const chunksb = self::chunksM * M_MILLION;
 	const maxCh   = 500;
 	
 	function __construct() {
 		kwas(is_readable(self::lfin), 'file not readable');
 		$sz = $this->thesz = filesize(self::lfin);
 		$rs = multi_core_ranges::get(1, $sz);
-		// $r = fopen($self::lfin, 'r');
-		$chb = self::chunksM * M_MILLION;
-		$ri = 0;
+		$r = fopen(self::lfin, 'r');
+		$rn = 0;
 		$ii = 0;
 		$si = 0;
-		do {
+		$remn = $sz;
+		
+		while($si++ < self::maxCh && $rn < $sz) {
+			if ($remn > self::chunksb) $tor = self::chunksb;
+			else					   $tor = $remn;
 			
-		} while($si++ < self::maxCh);
+			$t = fread($r, $tor);
+			try {
+				kwas(kwifs($t, $tor - 1) !== false && kwifs($t, $tor) === false, 'bad read n 2317');
+			} catch(Exception $ex) {
+				$l = strlen($t);
+				throw $ex;
+			}
+			$rn   += $tor;
+			$remn -= $tor;
+	
+			
+			continue;
+		} 
 		return; 
 		
 		
