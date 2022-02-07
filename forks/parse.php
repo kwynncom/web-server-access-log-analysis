e5c2d657055921dc66ad66323dc99c0e6ba82df6
kwynncom <buessresume@gmail.com>
2022-02-07 00:03:44 -0500

    progress - not sure faster, but fun

--- forks/parse.php
+++ forks/parse.php
@@ -7,65 +7,65 @@ class wsal_parse_in_file {
 		
 		$li = 0;
 		do {
-			
-			$this->ipf($h);
-			$this->uf($h);
-			$this->t10($h);
-			$this->do20($h);
 			$l = fgets($h); $li++;
-			if (!$l) {
-				exit(0);
-			}
-		} while($li < 100000);
+			if (!$l) break;
+			$this->do05($l);
+		}   while($li < 10000);
 		
 		return;
 	}
-	
-	function do20($h) {
-		fgetc($h);
-		$s = '';
 
-		for ($i=0; $i < 200; $i++) { 
-			$c = fgetc($h);
+	function do05($l) {
+		$i = 0;
+		$ip = '';
+		do { 
+			$c = $l[$i++];
+			if ($c === ' ') break;
+			else $ip .= $c; 
+		} while($i < 50);
+		
+		$i += 5;
+		
+		$hu = substr($l, $i, 26); $i += 26;
+		
+		$i += 2;
+		$msfri = substr($l, $i, 6); $i += 6;
+		
+		$i += 1;
+		
+		$s = '';
+		while ($i++ < 7000) { 
+			$c = $l[$i];
 			if ($c === '"') break;
 			$s .= $c;
 		}
 		
-		if ($s === '-') return; // 408
+		$cmd = $s; unset($s);
+		$cmdl = strlen($cmd);
 		
-		$a = explode(' ', $s);
-		try {
-			kwas(count($a) === 3, 'bad count wsal cmd 2313');
-		} catch (Exception $ex) {
-			kwynn();
-		}
+		if ($cmd !== '-') $acmd = explode(' ', $cmd);
 		
-		return;
-	}
-	
-	function t10($h) {
-		// 12345678901234567890123456
-		// 23/Oct/2021:01:38:34 -0400
-		$hu = fread($h, 26);
-		fread($h, 2);
-		$usfri = fread($h, 6);
-		fgetc($h);
-		return;
-		
-	}
-	
-	function uf($h) { fread($h, 5); }
-	
+		$i += 2;
+		$htc = substr($l, $i, 3); $i += 4;
 
-	function ipf($h) {
-		$i = 0;
-		$ip = '';
-		do { 
-			$c = fgetc($h);
-			if ($c === ' ') break;
-			else $ip .= $c; 
-		} while($i++ < 50);
+		if ($l[$i] !== '-') {
+			$l20 = substr($l, $i, 10);
+			preg_match('/^\d+/', $l20, $htrsza);
+
+			if (!isset($htrsza[0])) {
+				kwynn();
+			}
+
+			$i += strlen($htrsza[0]);
+		} else $i++;
 		
-		// $this->d->ip = $ip;
+		$l30 = substr($l, $i);
+		
+		preg_match_all('/"([^"]+)/', $l30, $ms);
+		
+		
+	//	exit(0);
+		return $i;
+
 	}
 }
