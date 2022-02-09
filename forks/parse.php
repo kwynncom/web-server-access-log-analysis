76a491b8a981714a5d412113b5871ee59f3376ab
kwynncom <buessresume@gmail.com>
2022-02-09 05:29:14 -0500

    lost coherence hours ago

--- forks/parse.php
+++ forks/parse.php
@@ -4,7 +4,7 @@ class wsal_parse_in_file {
 	
 	const charLimit = 10000; // some jerks will call ~8,000 char lines
 
-	public static function parse($l) {
+	public static function parse($l, $ti) {
 
 		$llen = strlen($l);
 
@@ -20,17 +20,24 @@ class wsal_parse_in_file {
 		
 		$hu = substr($l, $i, 26); $i += 26;
 		
+		if ($hu === "23/Oct/2021:01:38:34 -0400") {
+			kwynn();
+		}
+		
 		$i += 2;
 		$msfri = intval(substr($l, $i, 6)); $i += 6;
 		
-		$i += 1;
-		
-		$ci = $i;
+		$i += 2;
 		
 		$srf = false;
 		$s = '';
-		while ($i++ < self::charLimit) { 
-			$c = $l[$i];
+		while ($i < self::charLimit) { 
+			if (!isset($l[$i])) {
+				
+				kwynn();
+				return;
+			}
+			$c = $l[$i++];
 			if ($c === '"')
 				if ($l[$i - 1] !== '\\') break;
 				else $srf = true;
@@ -50,21 +57,36 @@ class wsal_parse_in_file {
 				$url  = $acmd[1];
 				if ($acmd[2] !== 'HTTP/1.1') $unusualHTV = $acmd[2];
 			} unset($acmd);
-		} unset($cmd);
+			
+			$i += 1;
+		} else {
+			kwynn();
+			$i += 1;
+		}
 
-		$i += 2;
+
 		$htrc = intval(substr($l, $i, 3)); $i += 4;
 
 		if ($l[$i] !== '-') {
-			preg_match('/^\d+/', substr($l, $i, 10) , $htrsza);
+			$ss = substr($l, $i, 10);
+			preg_match('/^\d+/', $ss , $htrsza);
+			if (!isset($htrsza[0])) {
+				kwynn();
+			}
 			$i   += strlen($htrsza[0]);
 			$rlen = intval($htrsza[0]); unset($htrsza);
 		}  else { $i++; $rlen = 0; }
-		
+		unset($cmd);
 		$l30 = substr($l, $i); unset($i, $ci);
 		
-		preg_match_all('/"([^"]+)/', $l30, $ms); unset($l30);
+		if (!preg_match_all('/"([^"]*)/', $l30, $ms)) {
+			kwynn();
+		}
+		unset($l30);
 		
+		if (!isset($ms[1][2])) {
+			kwynn();
+		}
 		$ref     = $ms[1][0];
 		$agent   = $ms[1][2]; 
 		
--- forks/parse20.php
+++ forks/parse20.php
@@ -1,17 +1,19 @@
 <?php
 
 require_once('/opt/kwynn/kwutils.php');
+require_once('parse.php');
 
-class wsal_parse_in_file {
+class wsal_parse_in_file_20 {
 		
 	const charLimit = 10000; // some jerks will call ~8,000 char lines
 	const lfin = '/var/kwynn/logs/a14M';
-	const chunks = 7 * M_MILLION;
-	const maxl = 2000000;
+	const chunks = 200000;
+	const maxl = 1;
+	const fileMax = M_BILLION;
 
 	public function __construct() {
 		$this->do10();
-		$this->parse();
+		$this->do20();
 	}
 	
 	private function do10() {
@@ -19,135 +21,39 @@ class wsal_parse_in_file {
 		$this->fsz  = filesize(self::lfin);
 		$this->fhan = fopen(self::lfin, 'r');
 	}
+
 	
-private function parse() {
-
-	// if not 0, then throw away rest of line, as with prev version
-
-	$bp = $i = $this->cfp;
-
-	$l = '';
-	
-	$ti = 0;
-	do {		
-		
-		if ($ti === 66745) {
-			kwynn();
-		}
-		
-		if (!isset($l[$i + self::charLimit])) {
-			$l  = substr($l, $i);
-			$blen = strlen($l);
-			$i  = 0;
-			if ($bp + self::chunks < $this->fsz) $tor = self::chunks;
-			else								 $tor = $this->fsz - ftell($this->fhan);
+	private function do20() {
+		$r = $this->fhan;
+		$totl = 0;
 			
-			if (ftell($this->fhan) >= $this->fsz) return;
+		do { 
+			
+			$ft = ftell($r);
+			if ($ft >= $this->fsz) return;
+			$rem = $this->fsz - $ft;
+			if (self::chunks < $rem) $tor = self::chunks;
+			else					 $tor = $rem;
 			
 			if ($tor <= 0) return;
-			try {
-				if (($ftell = ftell($this->fhan)) !== $bp) {
-					kwynn();
-				}
-				$buf = fread($this->fhan, $tor);
-				kwas(isset($buf[$tor - 1]), 'not enough read wsal buf 0359');
-			} catch(Exception $ex) {
-				$elen = strlen($buf);
-				kwynn();
-			}
-			$l .= $buf;
-			$blen = strlen($l);
 			
-			// if ($tor < self::chunks) exit(0);
-			kwynn();
-		}
-		
-		if (!isset($l[$i + 30])) return;
-		$ip = '';
-		for ($j=0; ($c = $l[$i + $j]) !== false; $j++) {
-			if ($c === ' ') break;
-			$ip .= $c; 
-		}
-		
-		if ($c === false) {
-			kwynn();
-		}
-		
-		unset($c); unset($tor);
-
-		$i += strlen($ip) + 6;
-
-		$hu = substr($l, $i, 26); $i += 26;
-
-		$i += 2;
-		$msfri = intval(substr($l, $i, 6)); $i += 6;
-
-		$i += 2;
-
-		$srf = false;
-		$s = '';
-		for ($j=0; $j < self::charLimit; $j++) { 
-			$c = $l[$i + $j];
-			if ($c === '"')
-				if ($l[$i + $j - 1] !== '\\') break;
-				else $srf = true;
-			$s .= $c;
-		} unset($c);
-		
-		$i += $j;
-
-		if ($srf) {
-			$s = str_replace('\"', "'", $s); 
-		} unset($srf);
-
-		$cmd = $s; unset($s);
-
-		if ($cmd !== '-') {
-			$acmd = explode(' ', $cmd); 
-			if (isset($acmd[2])) {
-				$verb = $acmd[0];
-				$url  = $acmd[1];
-				if ($acmd[2] !== 'HTTP/1.1') $unusualHTV = $acmd[2];
-			} unset($acmd);
-		} 
-
-		$i += 2;
-		$htrc = intval(substr($l, $i, 3)); $i += 4;
-
-		if ($l[$i] !== '-') {
-			$ss =  substr($l, $i, 10);
-			preg_match('/^\d+/', $ss, $htrsza);
-			if (!isset($htrsza[0])) {
-				kwynn();
+			$buf  = fread($r, $tor);
+			if (ftell($r) >= $this->fsz) return;
+			$buf .= fgets($this->fhan);
+			$line = strtok($buf, "\n");
+			$ti = 0;
+			while ($line) {
+				$len = strlen($line);
+				$totl += $len;
+				wsal_parse_in_file::parse($line, ++$ti);
+				$line = strtok("\n");
 			}
-			$i   += strlen($htrsza[0]);
-			$rlen = intval($htrsza[0]); unset($htrsza);
-		}  else { $i++; $rlen = 0; }
-		unset($cmd, $ss);
-		
-		$i += 2; // first "
-		for ($j=0; $l[$i + $j] !== '"'; $j++) ;
-		$ref = substr($l, $i, $j);
-		
-		$i += strlen($ref) + 3;
-	
-		for ($j=0; $l[$i + $j] !== '"'; $j++)  ;
-		$agent = substr($l, $i, $j);
-		
-		for ($j = 0; $l[$i + $j] !== "\n"; $j++) ;
+			
 
-		$i += $j + 1;
-		
-		if ($i < $bp) $line = substr($l, 0, $i);
-		else		  $line = substr($l, $bp, $i - $bp);
-		
-		echo($line);
+		} while ($totl < self::fileMax);
+	}
 	
-		$bp += strlen($line);
-	} while(++$ti < self::maxl);
-	
-	return;
-} // func
+
 } // class
 
-if (didCLICallMe(__FILE__)) new wsal_parse_in_file();
+if (didCLICallMe(__FILE__)) new wsal_parse_in_file_20();
