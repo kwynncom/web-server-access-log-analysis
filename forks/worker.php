7b44a1b12fb37dc14f1e1936c9a71776d2d4a94c
kwynncom <buessresume@gmail.com>
2022-02-10 01:09:09 -0500

    cleanup

--- forks/worker.php
+++ forks/worker.php
@@ -5,12 +5,10 @@ require_once('parse.php');
 
 class wsal_worker {
 		
-	const chunks = 500000;
-	const fileMax = M_BILLION;
+	const chunks  = 500000;
+	const nchunks =   1000;
 
-	public static function doit(...$thea) {
-		new self($thea);
-	}
+	public static function doit(...$thea) { new self($thea);}
 	
 	public function __construct($a5a) {
 		$this->low  = $a5a[0];
@@ -26,83 +24,67 @@ class wsal_worker {
 	}
 	
 	private function do10() {
-		$this->cfp  = $this->low;
+		$fp = $this->cfp  = $this->low;
 		$fsz = filesize($this->fnm);
 		$fts = $this->fts = filemtime($this->fnm);
 		$this->dhu = date('md-Hi-Y-s', $fts);
 		if ($this->high > $fsz) $this->high = $fsz;
-		$this->fhan = fopen($this->fnm, 'r');
+		$r = $this->fhan = fopen($this->fnm, 'r');
+		fseek($r, $fp);
 	}
 
+	private function chunkpp() {
+		$r = $this->fhan;
+		$p = ftell($r);
+		if ($p === 0) return '';
+		fseek($r, $p - 1);
+		if (fgetc($r) !== "\n") return fgets($r);
+		return '';
+	}
 	
 	private function do20() {
 		$r = $this->fhan;
-		$li = 0;
-		$fp = $this->cfp;
+		$chi = 0;
 				
-		fseek($r, $fp);
-		
 		do { 
-
 			$buf = '';
-			
-			if ($fp > 0) {
-				fseek($r, $fp - 1);
-				if (fgetc($r) !== "\n") {
-					fgets($r); // throw away
-
-				}
-			}
-			
-			$fpl = $fp = ftell($r);
-			
-			$rem = $this->high - $fp;
-			
+			$this->chunkpp();
+			$rem = $this->high - ftell($r);
 			if ($rem <= 0) break;
-			
 			if ($rem < self::chunks) $tor = $rem;
 			else					 $tor = self::chunks;
-			
 			$buf  = fread($r, $tor);
-			$bufsz = strlen($buf);
-			$fp += $bufsz;
+			$buf .= $this->chunkpp();
+			$this->lineLoop(strtok($buf, "\n"));
+
+		} while ($chi++ < self::nchunks);
+	}
 	
-			fseek($r, $fp - 1);
-			if (fgetc($r) !== "\n") {
-				$rl = fgets($r);
-				$fp += strlen($rl);
-				$buf .= $rl;
-			}
-			
-			$line = strtok($buf, "\n");
-
-			while ($line) {
-				++$li;
-				$fp0 = $fpl;
-				$llen = strlen($line);
-				$fpp1 = $fp0 + $llen;
-				
-				// $pa = [];
-				$pa = wsal_parse_2022_010::parse($line, $li, $this->rangen);
-				$this->put($li, $line, $fp0, $fpp1, $llen, $pa);
-				$line = strtok("\n");
-			}
-
-		} while ($fp < self::fileMax);
+	private function lineLoop($line) {
 		
+		static $lii = 0;
+		$fp0 = ftell($this->fhan);
+		
+		while ($line) {
+			++$lii;	
+			echo($line . "\n");
+			$llen = strlen($line);
+			$fpp1 = $fp0 + $llen;
+			$pa = [];
+//				$pa = wsal_parse_2022_010::parse($line, $lii, $this->rangen);
+			$this->put($lii, $line, $fp0, $fpp1, $llen, $pa);
+			$fp0 += $llen;
+			$line = strtok("\n");
+		}		
+						
 		$this->iob->ino('done - commit');
 	}
 	
 	private function put($li, $line, $fp0, $fpp1, $llen, $pa) {
-		
 		$rn = $this->rangen;
 		extract($pa); unset($pa);
 		$_id = sprintf('%02d', $this->rangen) . '-' . sprintf('%07d', $li) . '-' . $this->dhu;
-		$this->iob->ino(get_defined_vars());	
-		// exit(0);
-		
+		// $this->iob->ino(get_defined_vars());	
 	}
-
 } // class
 
-if (didCLICallMe(__FILE__)) new wsal_parse_in_file_20();
