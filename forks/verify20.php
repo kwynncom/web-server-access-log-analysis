3a3abfc66cd46f7c924372365a4051caa7fcb8d5
kwynncom <buessresume@gmail.com>
2022-02-20 22:32:19 -0500

    just before forker

--- forks/verify20.php
+++ forks/verify20.php
@@ -7,26 +7,23 @@ class wsal_verify_20 extends dao_generic_3 implements wsal_config {
 	
 	const vcoll = 'verify';
 	
-	public function __construct() {
+	public function __construct(bool $isw = false, int $low = -1, int $high = -1, int $workerN = -1) {
+		
 		parent::__construct(self::dbname);
 		$this->creTabs(self::vcoll);
-		$this->do10();
+		$vs = $this->init10();
+		$this->dbv($vs);
 	}
 	
-	private function do10() {
-		$ts = wsal_load_forks::getL1AndCk(self::lfin);
-		$q = "db.getCollection('lines').findOne({'ftsl1' : $ts}, {'sort' : {'fpp1': -1}, 'fpp1' : 1, '_id' : 0})";
-		$lptr = dbqcl::q(self::dbname, $q); unset($q);
-		$q = "db.getCollection('lines').find({ ftsl1 : $ts, fpp1: { \$lte: $lptr }}).sort({ fpp1 : 1})";
+	private function dbv($vsin) {
+		extract($vsin); unset($vsin);
+		$q = "db.getCollection('lines').find({ ftsl1 : $ftsl1, fpp1: { \$lte: $fpp1 }}).sort({ fpp1 : 1})";
 		$q .= '.forEach(function(r) { print(r.line.trim()); })';
 
-//			$s = dbqcl::q($db, $this->lwq, false, false, true, ' | openssl md4 ', true);
+		$md4_db = self::extractMD(dbqcl::q(self::dbname, $q, false, false, true, ' | openssl md4', true)); unset($q);
 
-		$md4_db = dbqcl::q(self::dbname, $q, false, false, true, ' | openssl md4', true); unset($q);
-		
-		$ftsl1 = $ts ; unset($ts);
+
 		$fp0 = 0;
-		$fpp1 = $lptr; unset($lptr);
 		
 		$at = time();
 		$atr = date('r', $at);
@@ -36,7 +33,24 @@ class wsal_verify_20 extends dao_generic_3 implements wsal_config {
 		$this->vcoll->insertOne($dat);
 				
 		print_r($dat);
-		return;
+		return;		
+	}
+	
+	private function init10() {
+		$ftsl1 = wsal_load_forks::getL1AndCk(self::lfin);
+		$q = "db.getCollection('lines').findOne({'ftsl1' : $ftsl1}, {'sort' : {'fpp1': -1}, 'fpp1' : 1, '_id' : 0})";
+	//	$lptr = dbqcl::q(self::dbname, $q); 
+		unset($q);
+		$fpp1 = 3000;
+				
+		// dofork(false, 1, 2, 'wsal_verify_20', $ftsl1, $fpp1);
+		
+		return get_defined_vars();
+	}
+	
+	public static function extractMD($s) {
+		preg_match('/\s[0-9a-f]{32}/', $s, $ms);
+		return trim($ms[0]);
 	}
 	
 }
