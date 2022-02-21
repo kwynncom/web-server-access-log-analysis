5910118dd29598d736e561dfa84e9727ed36d6a0
kwynncom <buessresume@gmail.com>
2022-02-21 00:03:57 -0500

    forking verify working

--- forks/verify20.php
+++ forks/verify20.php
@@ -6,14 +6,28 @@ require_once('forker.php');
 class wsal_verify_20 extends dao_generic_3 implements wsal_config {
 	
 	const vcoll = 'verify';
+	const tfpp1 = 999883;	
 	
-	public function __construct() {
+	// 				new $thecl(true, $i, $fargs);
+	public function __construct($isw = false, $wn = -1, $exas = false) {
+		
+		if ($exas) $exas = $exas[0];
 		
 		parent::__construct(self::dbname);
 		$this->creTabs(self::vcoll);
-		$vs = $this->init10();
-		$this->dbv($vs);
-		$this->fv($vs);
+
+		if (!$isw) {
+			$vs = $this->init10();
+			$this->fork($vs);
+			unset($vs);
+		} else if ($wn === 1) $this->dbv($exas);
+		  else if ($wn === 2) $this->fv ($exas);
+		
+		
+	}
+	
+	private function fork($vs) {
+		fork_roundRobin::dofork(true, 1, 2, 'wsal_verify_20', $vs);
 	}
 	
 	private function fv($vsin) {
@@ -25,16 +39,18 @@ class wsal_verify_20 extends dao_generic_3 implements wsal_config {
 		$c .= ' | openssl md4';
 		$c .= '"';
 		
-		$md4_f = self::extractMD(shell_exec($c)); unset($c);
+		$md4_v_f = self::extractMD(shell_exec($c)); unset($c);
 	
-		$datd = $dat = get_defined_vars();
-		unset($dat['_id']);
-		$this->vcoll->upsert(['_id' => $_id], $dat, 1, false);
+		$dat = get_defined_vars();
+
+		$this->upsert($dat);
 				
-		print_r($datd);
-		
-		print_r(get_defined_vars());
-		
+		print_r($dat);
+	}
+	
+	private function upsert($dat) {
+		$q = ['_id' => $dat['_id']];
+		$this->vcoll->upsert($q, $dat, 1, false);		
 	}
 	
 	private function dbv($vsin) {
@@ -42,36 +58,58 @@ class wsal_verify_20 extends dao_generic_3 implements wsal_config {
 		$q = "db.getCollection('lines').find({ ftsl1 : $ftsl1, fpp1: { \$lte: $fpp1 }}).sort({ fpp1 : 1})";
 		$q .= '.forEach(function(r) { print(r.line.trim()); })';
 
-		$md4_db = self::extractMD(dbqcl::q(self::dbname, $q, false, false, true, ' | openssl md4', true)); unset($q);
+		$md4_v_db = self::extractMD(dbqcl::q(self::dbname, $q, false, false, true, ' | openssl md4', true)); unset($q);
 
-		$datd = $dat = get_defined_vars();
-		unset($dat['_id']);
-		$this->vcoll->upsert(['_id' => $_id], $dat, 1, false);
-				
-		print_r($datd);
+		$dat = get_defined_vars();
+
+		$this->upsert($dat);				
+		print_r($dat);
 	
 	}
 	
 	private function init10() {
 		$ftsl1 = wsal_load_forks::getL1AndCk(self::lfin);
-		$q = "db.getCollection('lines').findOne({'ftsl1' : $ftsl1}, {'sort' : {'fpp1': -1}, 'fpp1' : 1, '_id' : 0})";
-		$fpp1 = dbqcl::q(self::dbname, $q); 
-		unset($q);
+		
+		if (!self::tfpp1) {
+			$q = "db.getCollection('lines').findOne({'ftsl1' : $ftsl1}, {'sort' : {'fpp1': -1}, 'fpp1' : 1, '_id' : 0})";
+			$fpp1 = dbqcl::q(self::dbname, $q); 
+			unset($q);
+		} else $fpp1 = self::tfpp1;
 		
 		$fp0 = 0;
 		$_id = date('m-d-H:i:s-Y', $ftsl1) . '_' . $fp0 . '_' . $fpp1;
+	
+		$this->upsert(['_id' => $_id]); // prevent the potential forking race condition
 		
 		return get_defined_vars();
 	}
 	
 	public static function extractMD($s) {
-		preg_match('/\s[0-9a-f]{32}/', $s, $ms);
-		return trim($ms[0]);
+		kwas(preg_match('/\s([0-9a-f]{32})/', $s, $ms), 'no md4 found wsal verify');
+		return trim($ms[1]);
 	}
 	
 	public static function shouldSplit (int $low, int $high, int $cpuCount) { return true; }
 
 	
+}
+
+class fork_roundRobin {
+	public static function dofork($reallyFork, $startat, $endat, $thecl, ...$fargs) {
+
+		$cpids = [];
+		for ($i=$startat; $i <= $endat; $i++) {
+		    $pid = -1;	
+			if ($reallyFork) $pid = pcntl_fork();
+			if ($pid === 0 || !$reallyFork) {
+				new $thecl(true, $i, $fargs);
+				if ($reallyFork) exit(0);
+			}  
+			$cpids[$i] = $pid;	
+		}
+
+		if ($reallyFork) for($i=$startat; $i <= $endat; $i++) pcntl_waitpid($cpids[$i], $status);
+	}
 }
 
 if (didCLICallMe(__FILE__)) new wsal_verify_20();
