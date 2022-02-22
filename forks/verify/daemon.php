419b1713cd55e375e3964eac00f55f5db141939c
kwynncom <buessresume@gmail.com>
2022-02-21 21:42:01 -0500

    small refinements

--- forks/verify/daemon.php
+++ forks/verify/daemon.php
@@ -1,23 +1,19 @@
 <?php
 
 require_once('/opt/kwynn/kwutils.php');
-require_once(__DIR__ . '/../' . 'config.php');
 
-class wsal_validator_daemon implements wsal_config {
+class wsal_validator_daemon {
 	
-	const thef = '/var/kwynn/logs/a500M';
-	
+	const thef = '/var/kwynn/logs/a400M';
+	const nchunks =   4000;
+	const chunks  = 500000;
+		
 	public function  __construct() {
 		$this->init10();
 		$this->setNs();
 		$this->do10();
 	}
-	
-	public function __destruct() {
-		fclose($this->pipr);
-		proc_close($this->md4pr);		
-	}
-	
+
 	private function init10() {
 		$pipesInit = [0 => ['pipe', 'r'], 1 => ['pipe', 'w']];
 		$this->md4pr = proc_open('openssl md4', $pipesInit, $pipes); unset($pipesInit);
@@ -36,26 +32,24 @@ class wsal_validator_daemon implements wsal_config {
 		$cki = 0;
 		$h = fopen(self::thef, 'r');
 		fseek($h, $this->fromn);
-		$ttorl = $this->ton - $this->fromn + 1;
-		$progl = 0;
-		$reml  = $ttorl;
-		
-		
+		$reml  = $this->ton - $this->fromn + 1;
+			
 		do {
 			if ($reml > self::chunks) $itl = self::chunks;
 			else					  $itl = $reml;
 			
 			$s = fread($h, $itl);
-			$progl += $itl;
 			$reml  -= $itl;
 			if (!$s) break;
 			fwrite($this->pipw, $s);
 			if ($reml <= 0) break;
-		} while(++$cki <= self::chunks);
-		
-		fclose($h);
-		fclose($this->pipw);
+		} while(++$cki <= self::nchunks);
+
+		fclose($this->pipw);		
 		echo(fgets($this->pipr));
+		fclose($h);
+		fclose($this->pipr);
+		proc_close($this->md4pr);
 	}
 }
 
