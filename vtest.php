a4b0db97a1f569d036538bb59f81aade2c6d9353
kwynncom <buessresume@gmail.com>
2022-03-03 02:49:38 -0500

    stdin version

--- vtest.php
+++ vtest.php
@@ -1,14 +1,9 @@
 <?php
 
 require_once('/opt/kwynn/kwutils.php');
-$fn = '/var/kwynn/logs/a10K';
-// $r = fopen($fn, 'r');       // forward   8302975721575955999
-$r = popen("tac $fn", 'r');    // backwards 8302975721575955999
-
+ 
 $fx = 0;
-
-while ($l = fgets($r)) {
-
+while ($l = fgets(STDIN)) {
 	$a = unpack('P*', $l);
 	$lx = 0;
 	foreach($a as $v) $lx ^= $v;
