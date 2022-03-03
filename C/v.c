da325cbda153f9c16a91f33a819af9d5c2d90da5
kwynncom <buessresume@gmail.com>
2022-03-03 04:56:51 -0500

    changed type name

--- C/v.c
+++ C/v.c
@@ -5,14 +5,14 @@ void main(void) {
     const int  readLimit = 8000;
     char lnbuf[readLimit + 1];
 
-    typedef signed long long XTYPE;
-    XTYPE  fx = 0;
-    XTYPE  lx = 0;
-    XTYPE c64 = 0;
+    typedef signed long long XORTY;
+    XORTY  fx = 0;
+    XORTY  lx = 0;
+    XORTY c64 = 0;
 
     while (fgets(lnbuf, readLimit, stdin) != NULL) {
         lx = 0;
-        for (i=0; c64 = (XTYPE) lnbuf[i]; i++) lx ^= c64 << (8 * (i % 8));
+        for (i=0; c64 = (XORTY) lnbuf[i]; i++) lx ^= c64 << (8 * (i % 8));
         fx ^= lx;
     }
     
