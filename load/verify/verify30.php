33d3f9fed33666c4180dffaf007cf320e8a920dd
kwynncom <buessresume@gmail.com>
2022-03-04 21:17:26 -0500

    more cleaning

--- load/verify/verify30.php
+++ load/verify/verify30.php
@@ -7,15 +7,13 @@ class wsal_verify_30 extends dao_generic_3 implements wsal_config {
 	const hashP = __DIR__ . '/../../C/a.out';
 
 	public function __construct(bool $worker = false, int $l = -1, int $h = -2, int $rn = -1, ...$aa) {
+		$this->initdb();
 		if (!$worker) $this->getLatest();
 		else if (1 || $rn < 3) $this->workit($l, $h, $aa);
 	}
 	
 	private function workit($l, $h, $aa) {
 		$ftsl1 = $aa[0][0];
-
-		parent::__construct(self::dbname);
-		$this->creTabs(self::colla);
 		
 		$q = ['$and' => [['ftsl1' => $ftsl1], ['fpp1' => ['$gte' => $l]], ['fpp1' => ['$lte' => $h]]]];
 		$c = $this->lcoll->findc($q);
@@ -31,22 +29,13 @@ class wsal_verify_30 extends dao_generic_3 implements wsal_config {
 		fclose($inh); proc_close($inpr);
 		file_put_contents($aa[0][1], $xor, FILE_APPEND);
 	}
-	
-	private function workit10($l, $h, $aa) {
-		$ftsl1 = $aa[0][0];
-		$q  = "db.getCollection('lines').find("; 
-		$q .= "{ \$and : [ {ftsl1 : $ftsl1}, {fpp1: { \$gte: $l }}, {fpp1 : { \$lte : $h  }}]})";
-		// echo($q . "\n");
-		$q .= '.forEach(function(r) { print(r.line.trim()); })';
-		$res = dbqcl::q(self::dbname, $q, false, false, true, ' | ' . self::hashP , true);
-		// $res = dbqcl::q(self::dbname, $q); 
-		echo($res . ' = db res' . "\n");
-		file_put_contents($aa[0][1], $res, FILE_APPEND);
+
+	private function initdb() {
+		parent::__construct(self::dbname);
+		$this->creTabs(self::colla);		
 	}
 	
 	private function getLatest() {
-		parent::__construct(self::dbname);
-		$this->creTabs(self::colla);
 		$la = $this->lcoll->findOne([], ['sort' => ['ftsl1' => -1, 'fpp1' => -1]]);
 		
 		echo($la['fpp1'] . ' = num chars / last pointer + 1' . "\n");
@@ -57,13 +46,11 @@ class wsal_verify_30 extends dao_generic_3 implements wsal_config {
 	
 	private function fxor($fn) {
 		$t = file_get_contents($fn);
-		echo("FILE\n" . $t);
 		$a = explode("\n", $t);
 		$xor = 0;
 		foreach($a as $v) {
 			if (!is_numeric($v)) continue;
 			$n = intval($v);
-			echo("$n = int val\n");
 			$xor ^= $n;
 		}
 		
