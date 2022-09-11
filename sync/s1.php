588a722b2586039ef18746b6b7f0062193b04fd2
kwynncom <buessresume@gmail.com>
2022-09-10 23:49:56 -0400

    objectified version

--- sync/s1.php
+++ sync/s1.php
@@ -2,44 +2,69 @@
 
 require_once('/opt/kwynn/kwutils.php');
 
-function getcmd($h, $cf = null) {
+class remoteBashSession {
 	
-	$b = '';
-	$ts = 0;
+	const pollius  = 10000;
+	const pollis   =	 0;
+	const timeoutS =	10;
+	const defaultLoginCmd = 'goa'; // goa is my name for ssh [-ipv] ubuntu@kwynn.com -i /path/loginKey.pem "$@"
+	const loginInitBytes = 100;
+	
+	public function __construct($loginCmd = self::defaultLoginCmd) { 
+		$this->makeConn($loginCmd);
+		$this->login();
+	}
+	
+	private function makeConn($cmd) {
+		$pdnonce = [0 => ['pipe', 'r'], 1 => ['pipe', 'w'], 2 => ['pipe', 'w']];
+		$io;
+		$this->procoh = proc_open($cmd, $pdnonce, $io); unset($pdnonce);
+		$this->ouh  = $io[0];
+		$this->inh  = $io[1]; unset($io);
+	}
+	
+	private function login() {
+		$this->getCmdResultI(function($b) { return isset($b[self::loginInitBytes]); });		
+	}
+	
+	public function __destruct() {
+		fwrite	  ($this->ouh, 'exit' . "\n");
+		fclose	  ($this->inh); 
+		fclose	  ($this->ouh);
+		proc_close($this->procoh);		
+	}
 
-	do {
-		while (is_resource($h) && !feof($h)) {
-			$ra = [$h]; $ignorea1 = $ignorea2 = [];
-			if (!stream_select($ra, $ignorea1, $ignorea2, 0, 10000)) break;
-			$c = fgets($h);
-			$b  .= $c;
-		} 
-		
-		if ($cf && $cf($b)) break;
-		
-	} while($ts++ < 1000);
+	private function getCmdResultI($cf = null) {
+
+		$h = $this->inh;
+		$b = '';
+		$ts = 0;
+		$enterus = microtime(1);
+
+		do {
+			while (is_resource($h) && !feof($h)) {
+				$ra = [$h]; $ignorea1 = $ignorea2 = [];
+				if (!stream_select($ra, $ignorea1, $ignorea2, self::pollis, self::pollius)) break;
+				$c = fgets($h);
+				$b  .= $c;
+			} 
+
+			if ($cf && $cf($b)) break;
+
+		} while(microtime(1) - $enterus < self::timeoutS);
+
+		echo(	$b);
+		return  $b;
+	}
 	
-	return $b;
+	public function getCmdRes($cmd, $f) {
+		$cmd .= "\n";
+		fwrite($this->ouh, $cmd);
+		return $this->getCmdResultI($f);
+		
+	}
+	
 }
 
-
-	if (1) {
-
-	$cmd = 'goa';
-
-	$pdnonce = [0 => ['pipe', 'r'], 1 => ['pipe', 'w'], 2 => ['pipe', 'w']];
-	$io;
-	$inpr = proc_open($cmd, $pdnonce, $io); unset($pdnonce);
-	$ouh  = $io[0];
-	$inh  = $io[1]; unset($io);
-	
-	echo(getcmd($inh, function($b) { return isset($b[400]); }));
-
-	fwrite($ouh, 'stat -c %s /var/log/apache2/access.log' . "\n");
-	echo(getcmd($inh, function($b) { return preg_match("/\d+\n/", $b); }));
-	
-	fwrite($ouh, 'exit' . "\n");
-	fclose($inh); 
-	fclose($ouh);
-	proc_close($inpr);
-}
+$o = new remoteBashSession();
+$o->getCmdRes('stat -c %s /var/log/apache2/access.log', function($b) { return preg_match("/\d+\n/", $b); });
